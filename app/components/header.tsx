1db96fccf20118b29ab2f3432684d8406756974a
Pedro <pedro@automatication.com>
2023-10-05 17:23:08 +0100

    Add intro buttons

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,11 +9,11 @@ export default function Header() {
   return (
     <header className='z-[999] relative'>
       <motion.div 
-        className='fixed top-o left-1/2 h-[4rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/3 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3rem] sm:w-[36rem] sm:rounded-full'
+        className="fixed top-0 left-1/2 h-[4rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/3 backdrop-blur-[0.5rem] sm:top-6 sm:h-[3rem] sm:w-[36rem] sm:rounded-full"
         initial={{ x: '-50%', y: -100, opacity: 0 }}
         animate={{ x: '-50%', y: 0, opacity: 1 }}
       />
-      <nav className='flex fixed left-1/2 h-12 -translate-x-1/2 sm:top-[1.5rem] sm:h-[initial] sm:py-0'>
+      <nav className='flex fixed top-0 left-1/2 h-10 -translate-x-1/2 sm:top-[1.5rem] sm:h-[initial] sm:py-0'>
         <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[1rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-3'>
           {links.map((link) => (
             <motion.li 
--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -3,8 +3,10 @@
 import Image from 'next/image';
 import React from 'react';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
 import me from '@/public/images/me.png';
-import pageproof from '@/public/images/pageproof-logo.svg';
 
 export default function Intro() {
   return (
@@ -33,6 +35,39 @@ export default function Intro() {
       >
         Hey there 👋, I&rsquo;m <b>Pedro</b>. I&rsquo;m a software engineer based in Portugal 🇵🇹 and I&rsquo;m currently working at <a href='https://pageproof.com' className='font-bold hover:underline'>PageProof</a> as a Junior Software Engineer.
       </motion.p>
+      <motion.div
+        className='flex flex-row flex-wrap items-center justify-center gap-4'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+      >
+        <Link 
+          href='#contact' 
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+          Contact me
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
+        </Link>
+        <a
+          href='/CV.pdf'
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/20'
+        >
+          Download CV <HiDownload className='opacity-70 group-hover:scale-110 transition'/>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/pedroprimor/'
+          target='_blank'
+          className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-900 active:scale-105 transition cursor-pointer border border-black/20'
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href='https://github.com/pprimor'
+          target='_blank'
+          className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-900 active:scale-105 transition cursor-pointer border border-black/20'
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
     </section>
   );
 }
