d83c0bb1b1389ee2f53ff037331ab890e7e56869
pprimor <pedro.primor@pm.me>
2026-06-09 17:52:42 +0100

    style(footer): replace heart attribution with copyright line

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-center sm:mt-28 sm:mb-10">
       <small className="text-gray-700 dark:text-gray-300">
-        Made with{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-        by Pedro Primor
+        © {new Date().getFullYear()} Pedro Primor
       </small>
       <br />
       <small className="text-gray-700 text-xs dark:text-gray-300">
@@ -16,9 +12,8 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Source code
-        </a>{" "}
-        available on GitHub
+          Source on GitHub
+        </a>
       </small>
     </footer>
   );
